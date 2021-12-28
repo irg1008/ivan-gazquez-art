68afd861e5e943dc2e3b733e307d59072f4eeaf7
irg1008 <ivansudevlop@gmail.com>
2021-12-28 21:58:36 +0100

    Added i18n switter and working with NEXT_LOCALE cookie

--- src/components/core/LangSwapper/LangSwapper.tsx
+++ src/components/core/LangSwapper/LangSwapper.tsx
@@ -1,5 +1,4 @@
-// import styles from './LangSwapper.module.css'
-import useTranslation from 'next-translate/useTranslation'
+import styles from './LangSwapper.module.css'
 import { useRouter } from 'next/router'
 import { ChangeEventHandler } from 'react'
 import { useCookies } from 'react-cookie'
@@ -10,19 +9,26 @@ const LangSwapper = () => {
 		{ NEXT_LOCALE: string }
 	>(['NEXT_LOCALE'])
 
-	const { lang } = useTranslation()
-	const { pathname, push } = useRouter()
+	const { pathname, push, locale, locales } = useRouter()
 
 	const switchLanguage: ChangeEventHandler<HTMLSelectElement> = (e) => {
 		const locale = e.target.value
 		push(pathname, undefined, { locale })
-		if (cookie.NEXT_LOCALE !== lang) setCookie('NEXT_LOCALE', locale)
+		if (cookie.NEXT_LOCALE !== locale)
+			setCookie('NEXT_LOCALE', locale, { path: '/' })
 	}
 
 	return (
-		<select onChange={switchLanguage} defaultValue={lang}>
-			<option value="en">EN</option>
-			<option value="es">ES</option>
+		<select
+			onChange={switchLanguage}
+			defaultValue={locale}
+			className={styles.locale}
+		>
+			{locales?.map((l) => (
+				<option key={l} value={l}>
+					{l}
+				</option>
+			))}
 		</select>
 	)
 }
