cceabea216eb1b56acf02cd765279c1c230224cf
irg1008 <ivansudevlop@gmail.com>
2022-01-03 01:06:36 +0100

    Removed playwright test

--- src/e2e/example.spec.ts
+++ src/e2e/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
 test('should navigate to the main page', async ({ page }) => {
-	await page.goto('/404')
-	await expect(page.locator('h1')).toContainText('404')
+	await page.goto('/')
+	await expect(page.locator('h1')).toContainText('title')
 })
