8139a0c7d47c98d02f76f3ebaeed8c2fc337e14a
irg1008 <ivansudevlop@gmail.com>
2022-01-14 22:12:23 +0100

    Trying to fix type rror on prismaclient

--- src/lib/prisma/client.ts
+++ src/lib/prisma/client.ts
@@ -1,12 +1,15 @@
 import { PrismaClient } from '@prisma/client'
-declare global {
-	// eslint-disable-next-line no-var
-	var prisma: PrismaClient | undefined
-}
 
-// Create only one prisma client instance for development
-const prisma = global.prisma ?? new PrismaClient()
+// declare global {
+// 	// eslint-disable-next-line no-var
+// 	var prisma: PrismaClient | undefined
+// }
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+// // Create only one prisma client instance for development
+// const prisma = global.prisma ?? new PrismaClient()
+
+// if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+
+const prisma = new PrismaClient()
 
 export default prisma
