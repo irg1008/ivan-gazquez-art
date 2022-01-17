e2797e3e46a8d7a008459bbca5a5025034110edc
irg1008 <ivansudevlop@gmail.com>
2022-01-17 17:07:56 +0100

    Added global prisma object

--- src/lib/prisma/client.ts
+++ src/lib/prisma/client.ts
@@ -1,15 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-// declare global {
-// 	// eslint-disable-next-line no-var
-// 	var prisma: PrismaClient | undefined
-// }
+declare global {
+	// eslint-disable-next-line no-var
+	var prisma: PrismaClient | undefined
+}
 
-// // Create only one prisma client instance for development
-// const prisma = global.prisma ?? new PrismaClient()
+// Create only one prisma client instance for development
+const prisma = global.prisma ?? new PrismaClient()
 
-// if (process.env.NODE_ENV !== 'production') global.prisma = prisma
-
-const prisma = new PrismaClient()
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
 export default prisma
