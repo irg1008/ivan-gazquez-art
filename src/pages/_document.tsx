ae288944248462ba158d277a215eef88e38056f1
irg1008 <ivansudevlop@gmail.com>
2022-02-02 01:40:22 +0100

    Added tag mannager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,6 +17,12 @@ class MyDocument extends Document {
 			<Html>
 				<Head></Head>
 				<body>
+					<noscript
+						dangerouslySetInnerHTML={{
+							__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NXJX3WJ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+						}}
+					></noscript>
 					<Main />
 					<NextScript />
 				</body>
