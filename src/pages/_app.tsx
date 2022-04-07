daa9f7ac5855937bb2e0ccfff8b4bd5f962eda4d
irg1008 <ivansudevlop@gmail.com>
2022-04-07 12:54:41 +0200

    Removed auth and prisma, not using any sessions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,33 +2,23 @@ import type { AppProps } from 'next/app'
 import { NextPageWithLayout } from 'next'
 import Head from 'components/meta/Head'
 import '../styles/index.css'
-import { SessionProvider } from 'next-auth/react'
-import { Session } from 'next-auth'
 import BaseLayout from 'layout/Base'
 import { GAScript } from 'lib/ga/GAScript'
 
 type AppPropsWithLayout = AppProps & {
 	Component: NextPageWithLayout
-	pageProps: {
-		session: Session
-	}
 }
 
-function MyApp({
-	Component,
-	pageProps: { session, ...pageProps },
-}: AppPropsWithLayout) {
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 	const Layout = Component.Layout ?? BaseLayout
 
 	return (
 		<>
 			<Head title="Default Title" description="Default Description" />
 			<GAScript />
-			<SessionProvider session={session}>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-			</SessionProvider>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
 		</>
 	)
 }
